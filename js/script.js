bac72477b23347af8728d969d347a9fdab403c74
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2022-05-29 20:19:08 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-/*---------- ヘッダー ---------- */
+/*---------- ボタン 背景 ---------- */
 $(".openbtn").click(function () {
   $(this).toggleClass('active');
   $("#g-nav").toggleClass('panelactive');
+  $(".circle-bg").toggleClass('circleactive');
 });
 
 $("#g-nav a").click(function () {
   $(".openbtn").removeClass('active');
   $("#g-nav").removeClass('panelactive');
+  $(".circle-bg").removeClass('circleactive');
 });
