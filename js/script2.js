fdaa2aad452611ec3228481cba58cde1beb1b61a
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2023-09-21 04:11:14 +0900

    Create script2.js

---
+++ js/script2.js
@@ -0,0 +1,59 @@
+$(function(){
+  /*=================================================
+  ハンバーガ―メニュー
+  ===================================================*/
+  // ハンバーガーメニューをクリックした時
+  $(document).on('click','.hamburger', function() {
+    // ハンバーガーメニューの共通処理を呼び出す
+    hamburger();
+  });
+  // メニューのリンクをクリックした時
+  $(document).on('click','#navi a', function() {
+    // ハンバーガーメニューの共通処理を呼び出す
+    hamburger();
+  });
+
+  /*=================================================
+  Inview（画面に表示されたタイミングで処理を実行）
+  ===================================================*/
+  // BBBが選ばれる理由（スライド左）
+  $('.inview-slide-left').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
+      // 要素が表示されたらslide-leftクラスを追加
+      $(this).stop().addClass('slide-left');
+    }
+  });
+  // BBBが選ばれる理由（スライド右）
+  $('.inview-slide-right').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
+      // 要素が表示されたらslide-rightクラスを追加
+      $(this).stop().addClass('slide-right');
+    }
+  });
+  // 受講生の声（ふきだし）
+  $('.inview-balloon').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
+      // 要素が表示されたらballoonクラスを追加
+      $(this).stop().addClass('balloon');
+    }
+  });
+});
+
+/*=================================================
+ハンバーガ―メニュー共通処理
+===================================================*/
+// ハンバーガーメニューをクリックした時とメニュー内のリンクをクリックした時の
+// 処理が同じなので処理を共通化する
+function hamburger() {
+  // toggleClassを使用することで、hamburgerクラスにactiveクラスが存在する場合は削除、
+  // 存在しない場合を追加する処理を自動で行ってくれる
+  $(document).toggleClass('.hamburger','active');
+
+  if ($('.hamburger').hasClass('.hamburger','active')) {
+    // hamburgerクラスにactiveクラスが存在する場合は、naviにもactiveクラスを追加する
+    $('#navi').addClass('active');
+  } else {
+    // hamburgerクラスにactiveクラスが存在しない場合は、naviからactiveクラスを削除する
+    $('#navi').removeClass('active');
+  }
+}
